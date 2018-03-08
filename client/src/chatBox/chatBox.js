c1fea917d27f8aa788d00538e83be52402416b43
Christian Fraijo <ChristianFraijo@Christians-MacBook-Pro.local>
2018-03-07 19:10:27 -0600

    added part of the chat box components

--- client/src/chatBox/chatBox.js
+++ client/src/chatBox/chatBox.js
@@ -1,45 +1,14 @@
-import React, { Component } from 'react'
-import { render } from 'react-dom'
-import { Launcher } from '../../src'
+import React, { Component } from 'react';
+import { Widget } from 'react-chat-widget';
 
-class Demo extends Component {
-
-    constructor() {
-        super();
-        this.state = {
-            messageList: messageHistory
-        };
-    }
-
-    _onMessageWasSent(message) {
-        this.setState({
-            messageList: [...this.state.messageList, message]
-        })
-    }
-
-    _sendMessage(text) {
-        if (text.length > 0) {
-            this.setState({
-                messageList: [...this.state.messageList, {
-                    author: 'them',
-                    type: 'text',
-                    data: { text }
-                }]
-            })
-        }
-    }
-
+class App extends Component {
     render() {
-        return (<div>
-            <Launcher
-                agentProfile={{
-                    teamName: 'react-live-chat',
-                    imageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png'
-                }}
-                onMessageWasSent={this._onMessageWasSent.bind(this)}
-                messageList={this.state.messageList}
-                showEmoji
-            />
-        </div>)
+        return (
+            <div className="App">
+                <Widget />
+            </div>
+        );
     }
 }
+
+export default App;
