75e8bd2fe9b9798c4085fd51f3b5e4a0f3970d4c
Christian Fraijo <ChristianFraijo@Christians-MacBook-Pro.local>
2018-03-07 20:51:20 -0600

    added some more components to chat

--- client/src/chatBox/chatBox.js
+++ client/src/chatBox/chatBox.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 import { Widget } from 'react-chat-widget';
 
-class App extends Component {
+class Chatbox extends Component {
+
+    constructor(){
+        super()
+        this.state = {
+            showComments: false
+        }
+    }
+
+    toggleComments(){
+        console.log("toggle comments");
+        this.setState({
+            showComments: !this.state.showComments
+        })
+    }
+
     render() {
         return (
-            <div className="App">
+            <div onClick={this.toggleComments.bind(this)} className="Chatbox" style={{color:'#fff', position:'fixed', bottom:0, right:0, background:'#000', width:320, padding:6}}>
                 <Widget />
             </div>
         );
     }
 }
 
-export default App;
+
+export default Chatbox;
--- client/src/main/api.js
+++ client/src/main/api.js
@@ -1,80 +1,80 @@
 
-var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
-var watsonRes = {};
-var watsonScore = {};
-var toneScore = [];
-var toneRes = [];
-var numTones;
+// var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+// var watsonRes = {};
+// var watsonScore = {};
+// var toneScore = [];
+// var toneRes = [];
+// var numTones;
 
-var toneAnalyzer = new ToneAnalyzerV3({
-    username: "a024676d-1867-42c5-9fac-5908cc52cf02",
-    password: "JEK6jBlRLqrN",
-    version_date: "2016-05-19",
-    url: 'https://gateway.watsonplatform.net/tone-analyzer/api/'
-});
+// var toneAnalyzer = new ToneAnalyzerV3({
+//     username: "a024676d-1867-42c5-9fac-5908cc52cf02",
+//     password: "JEK6jBlRLqrN",
+//     version_date: "2016-05-19",
+//     url: 'https://gateway.watsonplatform.net/tone-analyzer/api/'
+// });
 
-toneAnalyzer.tone(
-    {
-        tone_input: 'Greetings from Watson Developer Cloud!',
-        content_type: 'text/plain'
-    },
-    function (err, tone) {
-        if (err) {
-            console.log(err);
-        } else {
+// toneAnalyzer.tone(
+//     {
+//         tone_input: 'Greetings from Watson Developer Cloud!',
+//         content_type: 'text/plain'
+//     },
+//     function (err, tone) {
+//         if (err) {
+//             console.log(err);
+//         } else {
 
-            numTones = JSON.stringify(tone.document_tone.tone_categories[0].tones[0].tone_name.length, null, 2);
+//             numTones = JSON.stringify(tone.document_tone.tone_categories[0].tones[0].tone_name.length, null, 2);
 
-            for (var i = 0; i < numTones; i++) {
+//             for (var i = 0; i < numTones; i++) {
 
-                watsonRes = JSON.stringify(tone.document_tone.tone_categories[0].tones[i].tone_name, null, 2);
+//                 watsonRes = JSON.stringify(tone.document_tone.tone_categories[0].tones[i].tone_name, null, 2);
 
-                toneRes.push(watsonRes);
+//                 toneRes.push(watsonRes);
 
-            }
+//             }
 
-            i = 0;
+//             i = 0;
 
-            for (i = 0; i < numTones; i++) {
+//             for (i = 0; i < numTones; i++) {
 
-                watsonScore = parseFloat(tone.document_tone.tone_categories[0].tones[i].score);
+//                 watsonScore = parseFloat(tone.document_tone.tone_categories[0].tones[i].score);
 
-                toneScore.push(watsonScore);
+//                 toneScore.push(watsonScore);
 
-            }
-
-
-            var result = Math.max(...toneScore);
+//             }
 
 
-
-            console.log(result);
-
-            console.log(toneRes);
-
-            console.log(toneScore);
-
-            console.log(toneScore.indexOf(result))
-
-            console.log(toneRes[toneScore.indexOf(result)])
-
-            var watEmo = toneRes[toneScore.indexOf(result)
-
-        return watEmo;
-
-        }
-    }
-);
+//             var result = Math.max(...toneScore);
 
 
 
-// toneRes = toneRes[maxIndex];
+//             console.log(result);
 
-// var results = toneScore.indexOf(Math.max(toneScore));
+//             console.log(toneRes);
 
-// console.log(results);
+//             console.log(toneScore);
 
-module.export = ToneResults;
+//             console.log(toneScore.indexOf(result))
 
-// export api
+//             console.log(toneRes[toneScore.indexOf(result)])
+
+//             var watEmo = toneRes[toneScore.indexOf(result)]
+
+//         return watEmo;
+
+//         }
+//     }
+// );
+
+
+
+// // toneRes = toneRes[maxIndex];
+
+// // var results = toneScore.indexOf(Math.max(toneScore));
+
+// // console.log(results);
+
+// module.export = ToneResults;
+
+// // export api
 
