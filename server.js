db723a10430b2ddb02d2bdb18620a4cfbab2b738
Tanner Martin <twmartin1147@gmail.com>
2018-03-17 00:14:48 -0500

    Cleaned up

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes= require('./routes/api/index.js');
-const PORT = process.env.PORT || 3001;
-
+const PORT = process.env.PORT || 3001; 
+// const PORT = process.herokuapp.com || 3001;
 var app = express();
 
 
